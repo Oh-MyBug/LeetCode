59c43f80a3281ca9bbbdfbe17a0b43561d3611ff
oh-mybug <justleecong@163.com>
2020-07-24 01:57:18 +0800

    Update 62. 不同路径

--- src/com/ohmybug/fifty_2/Solution_62.java
+++ src/com/ohmybug/fifty_2/Solution_62.java
@@ -1,7 +1,5 @@
 package com.ohmybug.fifty_2;
 
-import com.ohmybug.fifty_1.Solution_6;
-
 import java.util.Arrays;
 
 /**
@@ -9,6 +7,23 @@ import java.util.Arrays;
  * Data:        2020/7/24
  * author:      Oh_MyBug
  * version:     V1.0
+ */
+/*
+62. 不同路径
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+问总共有多少条不同的路径？
+示例 1:
+输入: m = 3, n = 2
+输出: 3
+解释:
+从左上角开始，总共有 3 条路径可以到达右下角。
+1. 向右 -> 向右 -> 向下
+2. 向右 -> 向下 -> 向右
+3. 向下 -> 向右 -> 向右
+示例 2:
+输入: m = 7, n = 3
+输出: 28
  */
 public class Solution_62 {
     /*public int uniquePaths(int m, int n) {
