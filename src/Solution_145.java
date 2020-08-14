7b327dbf4f28c1de5958bab1756ac92347480ccd
oh-mybug <justleecong@163.com>
2020-08-15 02:16:17 +0800

    Update 145. 二叉树的后序遍历

--- src/Solution_145.java
+++ src/Solution_145.java
@@ -7,6 +7,20 @@ import java.util.List;
  * author:      Oh_MyBug
  * version:     V1.0
  */
+
+/*
+145. 二叉树的后序遍历
+给定一个二叉树，返回它的 后序 遍历。
+示例:
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [3,2,1]
+ */
 public class Solution_145 {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
