7ba0c74765e4827ea7820e3f95a536798ab5e58b
oh-mybug <justleecong@163.com>
2020-08-24 01:15:57 +0800

    Update 220. 存在重复元素 III

---
+++ src/Solution_220.java
@@ -0,0 +1,26 @@
+/**
+ * ClassName:   Solution_220
+ * Data:        2020/8/23
+ * author:      Oh_MyBug
+ * version:     V1.0
+ */
+
+/*
+220. 存在重复元素 III
+在整数数组 nums 中，是否存在两个下标 i 和 j，使得 nums [i] 和 nums [j] 的差的绝对值小于等于 t ，且满足 i 和 j 的差的绝对值也小于等于 ķ 。
+如果存在则返回 true，不存在返回 false。
+示例 1:
+输入: nums = [1,2,3,1], k = 3, t = 0
+输出: true
+示例 2:
+输入: nums = [1,0,1,1], k = 1, t = 2
+输出: true
+示例 3:
+输入: nums = [1,5,9,1,5,9], k = 2, t = 3
+输出: false
+ */
+public class Solution_220 {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        return false;
+    }
+}
