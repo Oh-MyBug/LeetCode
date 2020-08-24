0df39b9e584fabbb901f2192e93f6371985c72e3
oh-mybug <justleecong@163.com>
2020-08-25 02:38:51 +0800

    Update 236. 二叉树的最近公共祖先

---
+++ src/Solution_236.java
@@ -0,0 +1,123 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ClassName:   Solution_236
+ * Data:        2020/8/25
+ * author:      Oh_MyBug
+ * version:     V1.0
+ */
+
+/*
+236. 二叉树的最近公共祖先
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+示例 1:
+输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+输出: 3
+解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+示例 2:
+输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+输出: 5
+解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+ */
+public class Solution_236 {
+
+    public static void main(String[] args) {
+        TreeNode node0 = new TreeNode(0);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node8 = new TreeNode(8);
+
+        node3.left = node5;
+        node3.right = node1;
+        node5.left = node6;
+        node5.right = node2;
+        node2.left = node7;
+        node2.right = node4;
+        node1.left = node0;
+        node1.right = node8;
+
+        new Solution_236().lowestCommonAncestor(node3, new TreeNode(5), new TreeNode(1));
+    }
+
+    private List<TreeNode> res = null;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> p_path = null;
+        List<TreeNode> q_path = null;
+
+        List<TreeNode> temp = new ArrayList<>();
+        temp.add(root);
+
+        helper(root, p, temp);
+        p_path = new ArrayList<>(res);
+
+        res = null;
+        helper(root, q, temp);
+        q_path = new ArrayList<>(res);
+
+        System.out.println(p_path.toString());
+        System.out.println(q_path.toString());
+
+        int q_path_len = q_path.size();
+        int p_path_len = p_path.size();
+        while (q_path_len != p_path_len){
+            if (q_path_len > p_path_len)
+                q_path.remove(q_path_len - 1);
+            else
+                p_path.remove(p_path_len - 1);
+            q_path_len = q_path.size();
+            p_path_len = p_path.size();
+        }
+
+        while (q_path.get(q_path_len - 1) != p_path.get(p_path_len - 1)){
+            q_path.remove(q_path_len - 1);
+            p_path.remove(p_path_len - 1);
+            q_path_len = q_path.size();
+            p_path_len = p_path.size();
+        }
+
+        return p_path.remove(p_path_len - 1);
+    }
+
+    public void helper(TreeNode root, TreeNode target, List<TreeNode> temp) {
+        if (root.val == target.val) {
+            res = new ArrayList<>(temp);
+            return;
+        }
+        if (res != null) return;
+        if (root.left != null) {
+            temp.add(root.left);
+            helper(root.left, target, temp);
+            temp.remove(temp.size() - 1);
+        }
+        if (root.right != null) {
+            temp.add(root.right);
+            helper(root.right, target, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    '}';
+        }
+    }
+}
